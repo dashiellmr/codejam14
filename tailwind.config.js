90f4fe8ddc539865cb704877764e049f58d1970a
Sebastien Chow <svchow2003@gmail.com>
2024-11-23 19:13:39 -0500

    form style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,16 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    colors: {
+   
+    extend: { colors: {
       'beige': '#FFF8EE',
       'pink': '#FFEAEA' ,
       'green': '#CCECBF',
       'brown': '#D5B569',
-    },
-    extend: {},
+      'lightergreen': '#E4F2DE',
+      'lightblue':'#D5EEFF',
+      'darkblue': '#A5D6F9',
+    },},
   },
   plugins: [],
 }
